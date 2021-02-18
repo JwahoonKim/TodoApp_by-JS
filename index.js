8f3120b21bd001503fd9ccd6b88bad9da36ee954
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-02-18 23:01:55 +0900

    Test addEventListner

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const jhInfo = {
-    name : "KJH",
-    age : 33
+const title = document.querySelector("#title");
+
+const BASE_COLOR =  "white";
+const OTHER_COLOR = "green";
+
+function handleClick(){
+    const currenColor = title.style.color;
+    console.log(currenColor);
+    if (currenColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    }
+    else {
+        title.style.color = BASE_COLOR;
+    }
 }
 
-console.log(jhInfo)
+function init(){
+    title.style.color = BASE_COLOR;
+    window.addEventListener("click", handleClick);    
+}
+
+init();
