1b10955efc6634703ec25925e3cfcec2d4e931b0
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-02-18 23:32:03 +0900

    Test Toggle and classList

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const title = document.querySelector("#title");
 
-const BASE_COLOR =  "white";
-const OTHER_COLOR = "green";
+const CLICKED_CLASS = "clicked";
 
-function handleClick(){
-    const currenColor = title.style.color;
-    console.log(currenColor);
-    if (currenColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    }
-    else {
-        title.style.color = BASE_COLOR;
-    }
+function handleClick() {
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    title.classList.toggle(CLICKED_CLASS);
 }
 
 function init(){
-    title.style.color = BASE_COLOR;
-    window.addEventListener("click", handleClick);    
+    title.addEventListener("click", handleClick);    
 }
 
 init();
