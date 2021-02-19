d8ed54d4994da95d1fd8b081c66a72e9fd3e56b6
JwahoonKim <72662822+JwahoonKim@users.noreply.github.com>
2021-02-20 01:39:59 +0900

    메모기능 추가

--- greeting.js
+++ greeting.js
@@ -5,6 +5,22 @@ const greeting = document.querySelector(".js-greeting");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text){
     greeting.classList.add(SHOWING_CN);
     form.classList.remove(SHOWING_CN);
@@ -12,9 +28,9 @@ function paintGreeting(text){
 }
 
 function loadName(){
-    const currentUser = localStorage.getItem("USER_LS");
+    const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-
+        askForName();
     }
     else{
         paintGreeting(currentUser);
